0843bf6b89d21af723b3d8c1be978b9ae5ae5710
ZiadAhmedH <ziad.ahmedcs21@fcai.usc.edu.eg>
2024-07-18 19:57:48 +0300

    finish splash part

---
+++ lib/model/components/CustomText.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final Color? color;
+  final FontStyle? fontStyle;
+  final String? fontFamily;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+  final TextDecoration? decoration;
+  final Color? decorationColor;
+  final double? decorationThickness;
+  final TextBaseline? textBaseline;
+  const CustomText({super.key, 
+    required this.text,
+    this.fontSize,
+    this.fontWeight,
+    this.color,
+    this.fontStyle,
+    this.fontFamily,
+    this.textAlign,
+    this.maxLines,
+    this.overflow,
+    this.decoration, this.decorationColor, this.decorationThickness, this.textBaseline
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        decoration:decoration ,
+        decorationColor: decorationColor,
+        decorationThickness: decorationThickness,
+        textBaseline: textBaseline,
+        fontSize: fontSize ?? 14.0, // Default font size is 14.0 if not provided
+        fontWeight: fontWeight ?? FontWeight.normal, // Default is normal weight
+        color: color ?? Colors.black, // Default color is black if not provided
+        fontStyle: fontStyle ?? FontStyle.normal, // Default is normal style
+        fontFamily: fontFamily, // No default font family
+      ),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: overflow,
+    );
+  }
+}
--- lib/views/splashScreen.dart
+++ lib/views/splashScreen.dart
@@ -1,6 +1,8 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meeting_app/model/components/CustomText.dart';
+import 'package:meeting_app/utils/AppColor.dart';
 import 'package:meeting_app/views/HomePage.dart';
 import 'package:page_transition/page_transition.dart';
 
@@ -10,7 +12,14 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
-      splash: SvgPicture.asset("assets/logos/mainLogo.svg" , width: 200,),
+      splash: Center(
+        child: Row(
+          children: [
+            SvgPicture.asset("assets/logos/mainLogo.svg"),
+            const CustomText(text: "MeetSpace" , fontFamily: "Gilroy",fontWeight: FontWeight.bold,fontSize: 26,color: AppColor.white,)
+          ],
+        ),
+      ),
       nextScreen: HomePage(),
       splashTransition: SplashTransition.fadeTransition,
       duration: 3000,
