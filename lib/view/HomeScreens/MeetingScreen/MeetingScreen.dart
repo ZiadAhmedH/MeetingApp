d5e3b231e45f190e8c0f6f1b1f3388bc0e3c5a7f
ZiadAhmedH <ziad.ahmedcs21@fcai.usc.edu.eg>
2024-09-26 23:38:07 +0300

    " change UIKit"

--- lib/view/HomeScreens/MeetingScreen/MeetingScreen.dart
+++ lib/view/HomeScreens/MeetingScreen/MeetingScreen.dart
@@ -1,63 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_prebuilt_video_conference/zego_uikit_prebuilt_video_conference.dart';
 
 import '../../../utils/ZigoCloudConst.dart';
 import '../../../viewModel/bloc/MeetingCubit/meeting_cubit.dart';
 import '../../../viewModel/bloc/ProfileCubit/profile_cubit.dart';
 import '../../../viewModel/data/SharedKeys.dart';
 import '../../../viewModel/data/SharedPrefrences.dart';
-class MeetingScreen extends StatefulWidget {
+class MeetingScreen extends StatelessWidget {
   final String meetingId;
 
   const MeetingScreen({super.key, required this.meetingId});
-
-  @override
-  _MeetingScreenState createState() => _MeetingScreenState();
-}
-
-class _MeetingScreenState extends State<MeetingScreen> {
-  int minutes = 0;
-  int seconds = 0;
 
   @override
   Widget build(BuildContext context) {
     var profileCubit = ProfileCubit.get(context);
     var meetingCubit = MeetingCubit.get(context);
     return SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-            child: ZegoUIKitPrebuiltCall(
-              appID: ZigoCloud.ZEGO_APP_ID, // Fill in the appID from ZEGOCLOUD Admin Console.
-              appSign: ZigoCloud.ZEGO_APP_SIGN, // Fill in the appSign from ZEGOCLOUD Admin Console.
-              userID: LocalData.getData(key: SharedKey.uid),
-              userName: profileCubit.User?.userName! ?? 'Loading...',
-              callID: widget.meetingId,
-              events: ZegoUIKitPrebuiltCallEvents(
-                onCallEnd: (event, defaultAction) {
-                  defaultAction();
-                  Navigator.pop(context);
-                },
-              ),
-              onDispose: () {
-                meetingCubit.createMeeting(duration:"$minutes min  :  $seconds sec" );
-              },
-
-              config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
-                ..duration.onDurationUpdate = (Duration duration) {
-                  setState(() {
-                    minutes = duration.inMinutes;
-                    seconds = duration.inSeconds % 60;
-                  });
-                },
-            ),
+      child: ZegoUIKitPrebuiltVideoConference(
+        appID: ZigoCloud.ZEGO_APP_ID,
+        appSign: ZigoCloud.ZEGO_APP_SIGN,
+        userID:LocalData.getData(key: SharedKey.uid),
+        userName: LocalData.getData(key: SharedKey.email),
+        conferenceID: meetingId,
+        config: ZegoUIKitPrebuiltVideoConferenceConfig(
+          turnOnCameraWhenJoining: meetingCubit.isCameraOn,
+          turnOnMicrophoneWhenJoining: meetingCubit.isMicrophoneOn,
+          useSpeakerWhenJoining: meetingCubit.isSpeakerOn,
+          leaveConfirmDialogInfo: ZegoLeaveConfirmDialogInfo(
+            title: "Leave the Meeting?",
+            message: "Are you sure to leave the meeting ?",
+            cancelButtonName: "Cancel",
+            confirmButtonName: "Confirm",
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Duration: $minutes minutes and $seconds seconds'),
-          ),
-        ],
-      ),
+        ),
+      )
     );
   }
 }
