a6565e139ae7c7609fd3a5183201f641d5e441cf
ZiadAhmedH <Ziad.AhmedCS21@fcai.usc.edu.eg>
2025-04-17 01:36:37 +0200

    fix some gradle issues and soon working on Continue the App

--- lib/viewModel/bloc/VerfiyCubit/verfiy_cubit.dart
+++ lib/viewModel/bloc/VerfiyCubit/verfiy_cubit.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:meta/meta.dart';
-
 part 'verfiy_state.dart';
 
 class VerfiyCubit extends Cubit<VerfiyState> {
